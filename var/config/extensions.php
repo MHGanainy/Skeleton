31445e560cca593bd2749056f45780480140c7d2
mohamed hesham mohamed <mohamedhelganainy@gmail.com>
2021-09-28 18:14:36 +0100

    Installed PimcoreCustomerManagementFramework

--- var/config/extensions.php
+++ var/config/extensions.php
@@ -2,6 +2,8 @@
 
 return [
     "bundle" => [
-        "Pimcore\\Bundle\\EcommerceFrameworkBundle\\PimcoreEcommerceFrameworkBundle" => TRUE
+        "Pimcore\\Bundle\\EcommerceFrameworkBundle\\PimcoreEcommerceFrameworkBundle" => TRUE,
+        "CustomerManagementFrameworkBundle\\PimcoreCustomerManagementFrameworkBundle" => TRUE,
+        "Pimcore\\Bundle\\ObjectMergerBundle\\ObjectMergerBundle" => TRUE
     ]
 ];
